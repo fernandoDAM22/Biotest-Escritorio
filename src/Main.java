f18d9e66333a6a493306f890442eded26f1bbc18
Fernando <fernandoesmr@gmail.com>
2023-02-25 17:52:47 +0100

    añadir el icono a las ventanas, pero aun no se muestra

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
 import view.acceso.VentanaLogin;
 
@@ -7,10 +8,11 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-       try {
-            UIManager.setLookAndFeel(new FlatDarkLaf());
-        } catch( Exception ex ) {
-            System.err.println( "Failed to initialize LaF" );
+        try {
+            FlatLaf.setup(new FlatDarkLaf());
+            UIManager.put("Frame.icon", new ImageIcon(Main.class.getResource("/images/IconoVentana.ico")));
+        } catch (Exception ex) {
+            System.err.println("Failed to initialize LaF");
         }
         VentanaLogin frame = new VentanaLogin();
         frame.setVisible(true);
